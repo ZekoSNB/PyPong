10525f589f691a98607801e5c7ccb473a590ccd6
Adrian Tomasov <atomasov@redhat.com>
2022-03-05 11:19:42 +0100

    ball

--- pypong/ball.py
+++ pypong/ball.py
@@ -1,37 +1,32 @@
 from pypong.canvas_obj import CanvasObject
-import pygame
 
 
 class Ball(CanvasObject):
     def __init__(self, x, y, screen) -> None:
-        super().__init__()
-        self.x = x
-        self.y = y
-        self.src = screen
+        super().__init__(x, y, 25, 25, screen)
+        self.bspeed = 2.5
+        # self.bspeedy = 3
+        self.dir = self.Direction.UP | self.Direction.LEFT
 
-    def render(self):
-        self.rect = pygame.Rect(self.x, self.y, self.bsize, self.bsize)
-        pygame.draw.rect(self.src, self.rectcolor, self.rect)
-
-    def border(self):
-        if self.y >= (self.HEIGHT - self.bsize):
-            return 'up'
-        if self.y < 0:
-            return 'down'
-
-    def move(self, up, down, left, right):
-        if up and right:
-            self.x += self.bspeed
+    def move(self):
+        if self.dir & self.Direction.UP:
             self.y -= self.bspeed
-        if up and left:
-            self.x -= self.bspeed
-            self.y -= self.bspeed
-        if down and left:
-            self.x -= self.bspeed
+        if self.dir & self.Direction.DOWN:
             self.y += self.bspeed
-        if down and right:
+
+        if self.dir & self.Direction.LEFT:
+            self.x -= self.bspeed
+        if self.dir & self.Direction.RIGHT:
             self.x += self.bspeed
-            self.y += self.bspeed
 
     def reset(self):
         self.x = 400
+
+    def revert_dir(self):
+        self.dir = ~self.dir
+
+    def check_border(self, x, y):
+        if self.x <= 0 or self.x > x:
+            self.dir ^= self.Direction.LEFT | self.Direction.RIGHT
+        if self.y <= 0 or self.y > y:
+            self.dir ^= self.Direction.UP | self.Direction.DOWN
